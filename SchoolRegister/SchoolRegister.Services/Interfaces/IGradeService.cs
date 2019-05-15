56f095356d112936f62ca985da07ae3e2ff9dbc1
Konrad Gaj <kfocusek@gmail.com>
2019-05-16 00:27:43 +0200

    Added some Dtos classes and VMs classes

---
+++ SchoolRegister/SchoolRegister.Services/Interfaces/IGradeService.cs
@@ -0,0 +1,12 @@
+﻿using SchoolRegister.ViewModels.VMs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.Services.Interfaces
+{
+    public interface IGradeService
+    {
+      //  GradeVm AddOrUpdate() { }
+    }
+}
---
+++ SchoolRegister/SchoolRegister.ViewModels/DTOs/GetTeacherGroups.cs
@@ -0,0 +1,13 @@
+﻿using SchoolRegister.BLL.Entities;
+using System;
+using System.Linq.Expressions;
+
+
+namespace SchoolRegister.ViewModels.DTOs
+{
+    public class GetTeacherGroups
+    {
+        public int TeacherId { get; set; }
+        public Expression<Func<Student, bool>> GroupsFilterPredicate { get; set; }
+    }
+}
