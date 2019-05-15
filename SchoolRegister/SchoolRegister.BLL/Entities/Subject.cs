5b56f2fea5f65080edd3920f188f4c65cfa69996
Konrad Gaj <kfocusek@gmail.com>
2019-05-15 15:23:54 +0200

    Initial Service-oriented Architecture

--- SchoolRegister/SchoolRegister.BLL/Entities/Subject.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Subject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace SchoolRegister.BLL.Entities
     public class Subject
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public Teacher Teacher { get; set; }
         [ForeignKey("Teacher")]
