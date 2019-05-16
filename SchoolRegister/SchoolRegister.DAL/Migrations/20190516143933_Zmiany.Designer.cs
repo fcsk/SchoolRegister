6e6409ee0927e23dbb927f1fc3045c0ed4450fe8
Konrad Gaj <kfocusek@gmail.com>
2019-05-16 16:41:58 +0200

    StudentService.cs and new migration added for changes in BLL. Ex 3 is completed

--- SchoolRegister/SchoolRegister.DAL/Migrations/20190516143933_Zmiany.Designer.cs
+++ SchoolRegister/SchoolRegister.DAL/Migrations/20190516143933_Zmiany.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolRegister.DAL.EF;
 
 namespace SchoolRegister.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190516143933_Zmiany")]
+    partial class Zmiany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolRegister/SchoolRegister.Services/Services/StudentService.cs
@@ -0,0 +1,42 @@
+﻿using SchoolRegister.BLL.Entities;
+using SchoolRegister.DAL.EF;
+using SchoolRegister.Services.Interfaces;
+using SchoolRegister.ViewModels.VMs;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using AutoMapper;
+using System.Text;
+using System.Linq;
+
+namespace SchoolRegister.Services.Services
+{
+    public class StudentService : BaseService, IStudentService
+    {
+        private IEnumerable<StudentVm> studentsEntities;
+
+        public StudentService(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public StudentVm GetStudent(Expression<Func<Student, bool>> filterPredicate)
+        {
+            if (filterPredicate == null)
+            {
+                throw new ArgumentNullException($"filterPredicate is null");
+            }
+            var studentEntity = _dbContext.Users.OfType<Student>().FirstOrDefault(filterPredicate);
+            var studentVm = Mapper.Map<StudentVm>(studentEntity);
+            return studentVm;
+        }
+
+        public IEnumerable<StudentVm> GetStudents(Expression<Func<Student, bool>> filterPredicate)
+        {
+            var studentsEntities = _dbContext.Users.OfType<Student>().AsQueryable();
+            if (filterPredicate != null)
+                studentsEntities = studentsEntities.Where(filterPredicate);
+            var studentsVm = Mapper.Map<IEnumerable<StudentVm>>(studentsEntities);
+            return studentsVm;
+        }
+    }
+}
